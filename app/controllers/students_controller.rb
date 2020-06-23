5cd54b716e440fae7eb3cf7a35fd41dce16e7f8c
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-06-23 17:55:49 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,24 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: :show
   
   def index
     @students = Student.all
   end
 
-  def show
-    @student = Student.find(params[:id])
+  def new
   end
+
+  def create
+    Student.create(:first_name=>params[:student][:first_name], :last_name=>params[:student][:last_name])
+    redirect_to students_path
+  end
+
+  def show
+  end
+
+  private
+   def set_student
+    @student = Student.find(params[:id])
+   end
 
 end
